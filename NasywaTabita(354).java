a061ae5f7c4abed82ae1a601ecaa5cab1fa9f1d6
idrilll <scrtnmbr@gmail.com>
2024-05-27 16:05:10 +0700

    Create NasywaTabita(354).java

---
+++ NasywaTabita(354).java
@@ -0,0 +1,16 @@
+public class AnandaNasywaTabita(354) {
+
+    public static void main(String[] args) {
+        String fullname = "Nasywa Tabita Rasyid";
+        String nim = "2213010354";
+        String team = "Kelompok Java";
+        String role = "Marketer";
+
+        System.out.println("Halo, perkenalkan nama saya " + fullname + ".");
+        System.out.println("Nim saya = " + nim + ".");
+        System.out.println("Saya tergabung dalam kelompok " + team + ".");
+        System.out.println("Peran saya disini sebagai " + role + ".");
+    }
+
+}
+  
