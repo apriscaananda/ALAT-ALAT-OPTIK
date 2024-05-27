be59bbf5b6a0f1436861a58193dd9e37b932b911
apriscaananda <169017604+apriscaananda@users.noreply.github.com>
2024-05-27 15:50:22 +0700

    Update and rename AnandaAprisca.java to Aprisca(353).java

--- AnandaAprisca.java
+++
@@ -1,4 +0,0 @@
-E:.
-introduction
-  AnandaApriscaAnandaRosdianti.java
-  
---
+++ Aprisca(353).java
@@ -0,0 +1,16 @@
+public class AnandaApriscaAnandaRosdianty {
+
+    public static void main(String[] args) {
+        String fullname = "Aprisca Ananda Rosdianty";
+        String nim = "2213010349";
+        String team = "Kelompok Java";
+        String role = "Leader";
+
+        System.out.println("Halo, perkenalkan nama saya " + fullname + ".");
+        System.out.println("Nim saya = " + nim + ".");
+        System.out.println("Saya tergabung dalam kelompok " + team + ".");
+        System.out.println("Peran saya disini sebagai " + role + ".");
+    }
+
+}
+  
