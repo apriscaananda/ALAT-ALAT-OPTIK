45262e5d4c872e55dfc1cbcab242a657fb922c6f
BoH02 <jonathanaldo8@gmail.com>
2024-05-27 21:38:06 +0700

    2213010362(Jonathan Aldo Setiawwan)

---
+++ Jonathan(362).java
@@ -0,0 +1,13 @@
+public class Jonathan(362) {
+
+    public static void main(String[] args) {
+        String fullname = "Jonathan Aldo Setiawan";
+        String nim = "2213010362";
+        String team = "KELOMPOK JAVA";
+        String role = "Programmer";
+
+        System.out.println("Halo, perkenalkan nama saya " + fullname + ".");
+        System.out.println("Nim saya = " + nim + ".");
+        System.out.println("Saya tergabung dalam kelompok " + team + ".");
+        System.out.println("Peran saya disini sebagai " + role + ".");
+    }
